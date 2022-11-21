2cff4c7442b4888deb5dab0c8468ed662d485f7c
marbenMB <mbenbajj@student.1337.ma>
2022-11-21 05:15:05 +0100

    Generating Orthodox Canonical Form file Classes

--- CppProjects.sh
+++ CppProjects.sh
@@ -21,7 +21,57 @@ echo -n "Enter Class Name : "
 read CLASS
 if [ $CLASS != "" ]
 then
-	/usr/bin/touch main.cpp Makefile $CLASS.cpp $CLASS.hpp
+	/usr/bin/touch main.cpp Makefile $CLASS.cpp $CLASS.hpp 
+	echo "#ifndef _"$CLASS"_HPP_" >> $CLASS.hpp
+	echo "#define _"$CLASS"_HPP_" >> $CLASS.hpp
+	printf "\n" >> $CLASS.hpp
+	echo "#include <iostream>" >> $CLASS.hpp
+	printf "\n" >> $CLASS.hpp
+
+	echo "// ******************************************************** //" >> $CLASS.hpp
+	echo "//                         CLASSES                         //" >> $CLASS.hpp
+	echo "// ****************************************************** //" >> $CLASS.hpp
+
+	printf "\nclass	$CLASS
+{
+	public	:
+		$CLASS ();
+		$CLASS ($CLASS const &obj);
+		~$CLASS ();
+		$CLASS &operator= (const $CLASS &obj);
+
+	private	:
+		//	private attributes.
+};\n\n" >> $CLASS.hpp
+
+	printf "// ******************************************************** //
+//                        FUNCTIONS                        //
+// ****************************************************** //\n\n" >> $CLASS.hpp
+
+	printf "#include \"$CLASS.hpp\"
+
+$CLASS::$CLASS()
+{
+	std::cout << \"Default Constructor Called\" << std::endl;
+}
+
+$CLASS::~$CLASS()
+{
+	std::cout << \"Destructor Called\" << std::endl;
+}
+
+$CLASS::$CLASS($CLASS const &obj)
+{
+	std::cout << \"Copy Constructor Called\" << std::endl;
+}
+
+$CLASS	&$CLASS::operator= (const $CLASS &obj)
+{
+	std::cout << \"Copy Assignment Operator Called\" << std::endl;
+	
+	return (*this);
+}\n" >> $CLASS.cpp
+	echo "#endif" >> $CLASS.hpp
 	echo "+> " $CLASS " : Files Created !"
 else
 	echo "No Class Name Entred"
