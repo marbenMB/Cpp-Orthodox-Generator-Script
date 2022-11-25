1f4b3cc6c04b40c4323456a9a2e02485fd512278
marbenMB <mbenbajj@student.1337.ma>
2022-11-25 23:09:12 +0100

    Cpp Script : Updating cpp script : - Don't execute if path not accessible. - Check if user want to create main.cpp & makefile + Next update : - Create multiple class file by one execution. - if path not accessible redisplay the input filed to enter valid path (use loop). - if class name not valid redisplay the input filed until a valid class name.

--- CppProjects.sh
+++ CppProjects.sh
@@ -2,86 +2,91 @@
 
 PATH=""
 CLASS=""
+OPTION=""
 
 /usr/bin/clear
 
 #PATH
+echo "*******************************************************"
 echo -n "Enter Path Of Creation : "
 read PATH
 
 if [ -d $PATH ]
 then 
 	cd $PATH
-	echo $PATH
-	else
-		echo "***" $PATH  " : ** not found !! **"
-fi
+	echo "Changing Directory to : " $PATH
+	echo "*******************************************************"
+	echo -n "Enter Class Name : "
+	read CLASS
+	if [ $CLASS != "" ]
+	then
+		echo "Class Name : " $CLASS
+		echo "*******************************************************"
+		/usr/bin/touch $CLASS.cpp $CLASS.hpp 
 
-echo -n "Enter Class Name : "
-read CLASS
-if [ $CLASS != "" ]
-then
-	/usr/bin/touch main.cpp Makefile $CLASS.cpp $CLASS.hpp 
+		#	***		Printing To Class.hpp	***
 
-	#	***		Printing To Class.hpp	***
+		echo "#ifndef _"$CLASS"_HPP_" >> $CLASS.hpp
+		echo "#define _"$CLASS"_HPP_" >> $CLASS.hpp
+		printf "\n" >> $CLASS.hpp
+		echo "#include <iostream>" >> $CLASS.hpp
+		printf "\n" >> $CLASS.hpp
 
-	echo "#ifndef _"$CLASS"_HPP_" >> $CLASS.hpp
-	echo "#define _"$CLASS"_HPP_" >> $CLASS.hpp
-	printf "\n" >> $CLASS.hpp
-	echo "#include <iostream>" >> $CLASS.hpp
-	printf "\n" >> $CLASS.hpp
+		echo "// ******************************************************** //" >> $CLASS.hpp
+		echo "//                         CLASSES                         //" >> $CLASS.hpp
+		echo "// ****************************************************** //" >> $CLASS.hpp
 
-	echo "// ******************************************************** //" >> $CLASS.hpp
-	echo "//                         CLASSES                         //" >> $CLASS.hpp
-	echo "// ****************************************************** //" >> $CLASS.hpp
+		printf "\nclass	$CLASS
+	{
+		public	:
+			$CLASS ();
+			$CLASS ($CLASS const &obj);
+			~$CLASS ();
+			$CLASS &operator= (const $CLASS &obj);
 
-	printf "\nclass	$CLASS
-{
-	public	:
-		$CLASS ();
-		$CLASS ($CLASS const &obj);
-		~$CLASS ();
-		$CLASS &operator= (const $CLASS &obj);
+		private	:
+			//	private attributes.
+	};\n\n" >> $CLASS.hpp
 
-	private	:
-		//	private attributes.
-};\n\n" >> $CLASS.hpp
+		printf "// ******************************************************** //
+	//                        FUNCTIONS                        //
+	// ****************************************************** //\n\n" >> $CLASS.hpp
 
-	printf "// ******************************************************** //
-//                        FUNCTIONS                        //
-// ****************************************************** //\n\n" >> $CLASS.hpp
+		echo "#endif" >> $CLASS.hpp
 
-	echo "#endif" >> $CLASS.hpp
+		#	***		Printing To Class.cpp	***
 
-	#	***		Printing To Class.cpp	***
+		printf "#include \"$CLASS.hpp\"
 
-	printf "#include \"$CLASS.hpp\"
+	$CLASS::$CLASS()
+	{
+		std::cout << \"Default Constructor Called\" << std::endl;
+	}
 
-$CLASS::$CLASS()
-{
-	std::cout << \"Default Constructor Called\" << std::endl;
-}
+	$CLASS::~$CLASS()
+	{
+		std::cout << \"Destructor Called\" << std::endl;
+	}
 
-$CLASS::~$CLASS()
-{
-	std::cout << \"Destructor Called\" << std::endl;
-}
+	$CLASS::$CLASS($CLASS const &obj)
+	{
+		std::cout << \"Copy Constructor Called\" << std::endl;
+	}
 
-$CLASS::$CLASS($CLASS const &obj)
-{
-	std::cout << \"Copy Constructor Called\" << std::endl;
-}
+	$CLASS	&$CLASS::operator= (const $CLASS &obj)
+	{
+		std::cout << \"Copy Assignment Operator Called\" << std::endl;
+		
+		return (*this);
+	}\n" > $CLASS.cpp
+	echo -n "Create main.cpp and Makefile [y/n] : "
+	read OPTION
+	if [ $OPTION == "y" ]
+	then
+		/usr/bin/touch main.cpp Makefile
+		#	***		Printing To Makefile	***
 
-$CLASS	&$CLASS::operator= (const $CLASS &obj)
-{
-	std::cout << \"Copy Assignment Operator Called\" << std::endl;
-	
-	return (*this);
-}\n" >> $CLASS.cpp
-
-	#	***		Printing To Makefile	***
-
-	printf "NAME = $CLASS \n
+			printf "NAME = $CLASS \n
 CXXFLAGS = -Wall -Wextra -Werror \n
 CC = c++ \n
 STD = -std=c++98 \n
@@ -92,8 +97,8 @@ all : \$(NAME) \n
 \$(NAME) : \$(OBJ)
 	\$(CC) \$(CXXFLAGS) \$(STD) \$(OBJ) -o \$(NAME) \n\n" >> Makefile
 
-echo %.o : %.cpp "\$(INC)" >> Makefile
-printf "	\$(CC) \$(CXXFLAGS) -c \$< -o \$@ \n
+		echo %.o : %.cpp "\$(INC)" > Makefile
+		printf "	\$(CC) \$(CXXFLAGS) -c \$< -o \$@ \n
 clean :
 	rm -rf \$(OBJ) \n
 fclean : clean
@@ -101,17 +106,20 @@ fclean : clean
 re : fclean all \n
 .PHONY : all clean fclean re\n" >> Makefile
 
-	#	***		Printing To main.cpp	***
+			#	***		Printing To main.cpp	***
 
-	printf "#include \"$CLASS.hpp\"\n
+			printf "#include \"$CLASS.hpp\"\n
 int main()
 {
 	//	You Code\n
 	std::cout << \"******	MAR_BEN CREATION √	******\" << std::endl;\n
 	return (0);
-}\n" >> main.cpp
-
-	echo "+> " $CLASS " : Files Created !"
-else
-	echo "No Class Name Entred"
+}\n" > main.cpp
+	fi
+		echo "+> " $CLASS " : Files Created !"
+	else
+		echo "No Class Name Entred"
+	fi
+	else
+		echo "***" $PATH  " : ** not found !! **"
 fi
