0142bcf420cbcb9b764d01c1ece49956ee2618ea
marbenMB <mbenbajj@student.1337.ma>
2022-11-25 23:56:43 +0100

    Adding Script Installer

---
+++ Installer.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo ""
+echo "-------------------------------------------------------------"
+echo "		 ******"
+echo -e "\x1b[32m+> Usage : use > CppGen < command to run script anywhere!!\033[m"
+echo "		 ******"
+echo "-------------------------------------------------------------"
+echo ""
+echo -e "\x1b[32m ... Installation ... \033[m"
+echo -e "\x1b[33m ..... Updating ..... \033[m"
+git pull
+
+if [ $? == "0" ]
+then
+	echo -e "\x1b[32m ... Update DONE ... \033[m"
+else
+	echo -e "\x1b[31m ... Update FAILED ... \033[m"
+fi
+
+echo "alias CppGen='bash ~/Desktop/MakeGit_Script/CppProjects.sh'" >> ~/.zshrc
+if [ $? == "0" ]
+then
+	echo -e "\x1b[32m ... Installation DONE ... \033[m"
+else
+	echo -e "\x1b[31m ... Installation FAILED ... \033[m"
+fi
